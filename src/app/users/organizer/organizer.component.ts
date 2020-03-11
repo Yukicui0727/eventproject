c172cbe13bcfe0c8c5ef373640884cb5d2a2e932
Yukicui <Yukicui0727@gmail.com>
2020-03-11 14:58:36 -0700

    Account Information

--- src/app/users/organizer/organizer.component.ts
+++ src/app/users/organizer/organizer.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Organizer } from './organizer';
 
 @Component({
   selector: 'app-organizer',
   templateUrl: './organizer.component.html',
   styleUrls: ['./organizer.component.scss']
 })
-export class OrganizerComponent implements OnInit {
+export class OrganizerComponent {
 
-  constructor() { }
+  
 
-  ngOnInit(): void {
-  }
+  model = new Organizer(339, 'bunny9','bunny9@gmail.com', 'abc','bunny','12345678','uuu123.com','uoo','111','bellevue','WA','98005');
 
+  submitted = false;
+
+  onSubmit() { this.submitted = true; }
+  Cancel() { this.submitted = false; }
+ 
+  get diagnostic() { return JSON.stringify(this.model); }
 }
---
+++ src/app/users/organizer/organizer.ts
@@ -0,0 +1,18 @@
+export class Organizer {
+
+    constructor(
+      public id: number,
+      public accountname: string,
+      public emailaddress: string,
+      public otherdetails: string,
+      public contactname: string,
+      public telephone: string,
+      public websiteaddress: string,
+      public street1: string,
+      public street2: string,
+      public city: string,
+      public state: string,
+      public zipcode: string
+    ) {  }
+  
+  }
